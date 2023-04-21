2c3160eee53ee18b600e83c6dd64899d9515baa7
Josue Marquez <owaruuu@gmail.com>
2023-04-21 19:19:48 -0400

    fix local path

--- assets/js/estadisticas.js
+++ assets/js/estadisticas.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         responsive: true,
         language: {
             url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/es-CL.json',
-        }, ajax: 'assets/data/data.json',
+        }, ajax: './assets/data/data.json',
         columns: [
             { data: 'id' },
             { data: 'first_name' },
